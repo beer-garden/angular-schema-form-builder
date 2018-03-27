90acdc89877295616a3c054b6ed703b6f96898aa
Matt Patrick <hazmat345@gmail.com>
2018-03-27 11:43:14 -0400

    Moving snapshot testing

---
+++ test/snapshot/builder.test.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+import {sfBuilderService} from '../../src/builder';
+
+let sut, utilityStub;
+let jsonDir = path.join(__dirname, 'data');
+let jsonData = {};
+
+let files = fs.readdirSync(jsonDir);
+for (var systemFile of files) {
+  if (systemFile.endsWith('.json')) {
+    let systemName = systemFile.slice(0, -5);
+    jsonData[systemName] = {system: null, commands: {}};
+
+    // Load in system definition
+    jsonData[systemName]['system'] =
+      JSON.parse(fs.readFileSync(path.join(jsonDir, systemFile), 'utf8'));
+
+    let commandFiles = fs.readdirSync(path.join(jsonDir, systemName));
+    for (var commandFile of commandFiles) {
+      jsonData[systemName]['commands'][commandFile.slice(0, -5)] = JSON.parse(
+          fs.readFileSync(path.join(jsonDir, systemName, commandFile), 'utf8'));
+    }
+  }
+}
+
+describe('Schemaform builder', () => {
+
+  beforeEach(() => {
+    utilityStub = {
+      escapeHtml: sinon.stub().callsFake(input => input)
+    };
+    sut = sfBuilderService(utilityStub);
+  });
+
+  for (let system in jsonData) {
+    for (let command in jsonData[system]['commands']) {
+
+      it('should generate the schema and form', () => {
+        jestExpect(sut.build(
+          jsonData[system]['system'],
+          jsonData[system]['commands'][command]
+        )).toMatchSnapshot(system + ': ' + command);
+      });
+    }
+  }
+
+});
